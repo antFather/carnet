352e51afb75dec0dea4fdb7e85003a90f008a025
Antfather <925627037@qq.com>
2018-06-12 01:56:51 +0800

    shop net

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//$(".banner_box").slide({
+//    titCell: ".icon li",
+//    mainCell: ".banner",
+//    autoPlay: 'true',
+//    depayTime: 2500,
+//    effect: 'leftLoop',
+//})
+
+$(".banner_box").slide({
+    mainCell: ".banner",
+    autoPlay: 'true',
+    interTime: 3000,
+    effect: 'leftLoop',
+});
+
+$(".hot_box").slide({
+    mainCell: ".hot",
+    effect: 'leftLoop',
+	interTime:3000,
+	vis:6,
+    autoPlay: 'true',
+});
+
+$(".snacks_top_box").slide({
+    mainCell: ".snacks_top",
+    effect: 'leftLoop',
+	interTime:3000,
+	vis:3,
+    autoPlay: 'true',
+});
+
+$(".snacks_down_box").slide({
+    mainCell: ".snacks_down",
+    effect: 'leftLoop',
+	interTime:3000,
+	vis:3,
+    autoPlay: 'true',
+});
+
+
